a5812b8acadaf76937c08cb361b77bd4384292aa
Zeyu Wang <wzy.franklin@gmail.com>
2025-03-04 22:59:01 +1100

    Update AdvancedStockPriceTracker.cs

--- SystemDesignCoding/Coding.DataStructure/StockPrice/AdvancedStockPriceTracker.cs
+++ SystemDesignCoding/Coding.DataStructure/StockPrice/AdvancedStockPriceTracker.cs
@@ -35,7 +35,7 @@ public class AdvancedStockPriceTracker: IStockPriceTracker
 
     public int GetMaxPriceBefore(string timestamp)
     {
-        if (!DateTime.TryParse(timestamp, out var time))
+        if (!DateTime.TryParse(timestamp, out var parsedTime))
         {
             throw new ArgumentException("Invalid Timestamp");
         }
@@ -44,25 +44,15 @@ public class AdvancedStockPriceTracker: IStockPriceTracker
         {
             return -1;
         }
-        var low = 0;
-        var high = _timestampToPriceList.Count - 1;
-        while (low <= high)
-        {
-            var mid = low + (high - low) / 2;
-            if (_timestampToPriceList.Keys[mid] <= time)
-            {
-                low = mid + 1;
-            }
-            else
-            {
-                high = mid - 1;
-            }
-        }
 
-        if (high < 0)
+        var maxPrice = _timestampToPriceList.First().Value;
+        foreach (var (time, price) in _timestampToPriceList)
         {
-            return -1;
+            if (time <= parsedTime)
+            {
+                maxPrice = Math.Max(maxPrice, price);
+            }
         }
-        return _timestampToPriceList.Values.Take(high + 1).Max();
+        return maxPrice;
     }
 }
